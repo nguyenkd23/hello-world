4bb38a946e4d1d78b13fc004b9c448ee75e811bc
Khoa Nguyen <46753665+nguyenkd23@users.noreply.github.com>
2019-01-16 10:33:23 -0500

    new

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*************************************************************************
+* HelloPrinter.java
+**************************************************************************
+* ------------------------------------------------------------------------
+* Khoa Nguyen
+* 1/16/2019
+* CMSC 255 003
+*************************************************************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Khoa Nguyen");
+		System.out.println("HelloPrinter");
+		System.out.println("Version Date");
+		System.out.println("CMSC 255 003");
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
+
+
